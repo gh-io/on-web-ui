aeeeeff77601a987b4241b69848cb5919f397d6a
Seriki yakub <sandraisom00@gmail.com>
2025-12-31 03:21:42 +0100

    Update Clone.sh

--- Clone.sh
+++ Clone.sh
@@ -6,3 +6,24 @@ $ cd on-web-ui
 $ npm install
 $ npm start             # Start development server at http://127.0.0.1:3000.
 $ npm test              # Run automated tests.
+# development
+npm run server
+# production
+npm run build:prod
+npm run server:prod
+$ npm run build-js      # Build production `static/monorail.js`.
+$ npm run build-css     # Build minified `static/monorail.css` from `less/monorail.less`.
+$ npm run build         # Build `static/monorail.css` & `static/monorail.js` for production.
+
+$ nvm use 16
+Now using node v16.9.1 (npm v7.21.1)
+$ node -v
+v16.9.1
+$ nvm use 14
+Now using node v14.18.0 (npm v6.14.15)
+$ node -v
+v14.18.0
+$ nvm install 12
+Now using node v12.22.6 (npm v6.14.5)
+$ node -v
+v12.22.6
