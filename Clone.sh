760c3bef1eece59a2e12e83e4d52f69243cd0818
Seriki yakub <sandraisom00@gmail.com>
2025-12-31 03:18:33 +0100

    Update Clone.sh

--- Clone.sh
+++ Clone.sh
@@ -1,2 +1,8 @@
 $ gh repo clone Web4application/Web4-playground
 cd web4-playground
+
+$ git clone https://github.com/RackHD/on-web-ui.git
+$ cd on-web-ui
+$ npm install
+$ npm start             # Start development server at http://127.0.0.1:3000.
+$ npm test              # Run automated tests.
